0abfe7b7cd261e2d4a6af43158aac02e800c3b09
Kyle Richey <richey.kyle.i@gmail.com>
2018-01-09 08:55:35 -0800

    building basic app

---
+++ lab-kyle/src/components/home/index.js
@@ -0,0 +1,14 @@
+import './_home.scss';
+
+import React from 'react';
+
+class Home extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Fun With Fonts!</h1>
+      </div>
+    )
+  }
+}
+export default Home;
