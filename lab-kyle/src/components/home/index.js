f0aa62600220412bc7ad1e62a9be99a748597d00
Kyle Richey <richey.kyle.i@gmail.com>
2018-01-20 23:21:53 -0800

    font awesome done, lab done

--- lab-kyle/src/components/home/index.js
+++ lab-kyle/src/components/home/index.js
@@ -1,12 +1,15 @@
 import './_home.scss';
 
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/fontawesome-free-solid';
 
 class Home extends React.Component {
   render(){
     return (
       <div>
         <h1>Home!</h1>
+        <FontAwesomeIcon icon={faCoffee} />
       </div>
     )
   }
